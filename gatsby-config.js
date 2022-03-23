3cd535803b4e9a8bd212ad9cf92ee982210645aa
mehmeterenballi <mehmeterenballi@gmail.com>
2022-03-24 01:55:45 +0300

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,31 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-transformer-remark`,
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects/`,
+      },
+    },
+  ],
+  siteMetadata: {
+    title: 'Web  Warrior',
+    description: 'web dev portfolio',
+    copyripght: 'This website is copyright 2021 Web Warrior',
+    contact: 'me@webwarriortr.com'
+  }
 }
---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default function about() {
+    return (
+        <Layout>
+            <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto quo fugit laboriosam ducimus quaerat sed, dolor reprehenderit doloribus eius officiis vero laudantium quasi inventore recusandae tenetur, ea earum, obcaecati adipisci!</p>
+        </div>
+        </Layout>
+    )
+}
